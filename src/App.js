6dde91f93efdc7c5c07c8a71d647e6e533194c52
Md. Abbas Ali <alabbashossain@gmail.com>
2021-02-28 19:48:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import User from './Components/User/User';
+import data from './data/data.json';
+
+function App() {
+  const [user, setUser] = useState([])
+  const [addFriend, setAddFriend] = useState([]);
+  const handleAddFriend = (user) => {
+    const setNewFriend=[...addFriend,user]
+    setAddFriend(setNewFriend);
+  };
+
+  useEffect(() => {
+    setUser(data)
+    
+  }, [])
+  return (
+    <div>
+      <div>
+        <User userData={user} handleFriend={handleAddFriend} getFriendInfo={addFriend}></User>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+export default App;
